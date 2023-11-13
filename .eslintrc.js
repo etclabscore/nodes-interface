4724d4327fee15a528bcfe452afcf870bd9a9e60
Chris Ziogas <ziogaschr@gmail.com>
2023-11-13 13:17:45 +0200

    Upgrade nuxt.js@2.15.8 and fix lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,17 @@ module.exports = {
   root: true,
   env: {
     browser: true,
+    'jest/globals': true,
     node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
   },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: ['prettier'],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: ['jest'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://github.com/nuxt-community/eslint-module
+    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
